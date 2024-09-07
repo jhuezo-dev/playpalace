17f0fb1809083cec609b1a837768d3650f454510
jbhuezo1 <jbhuezo@alaska.edu>
2024-09-07 11:51:45 -0700

    converted page text to dynamic inserted text

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+/* Welcome page*/ 
+export const WelcomeTitle = `Welcome to Play Palace Alaska`
+export const WelcomeSubtitle = `Located inside of the Midtown Mall`
+export const WelcomeCardOneLinkText = `Rules, sanitation, and safety`
+export const WelcomeCardOneBodyText = `At Play Palace we want the safest and cleanest experience for your little ones. Take a look at our rules and how we strive to keep our palace a clean place.`
+export const WelcomeCardTwoLinkText = `Pricing`
+export const WelcomeCardTwoBodyText = `Do you have an event coming up soon? Look no further as we provide party packages.`
+export const WelcomeCardThreeLinkText = `View our Palace`
+export const WelcomeCardThreeBodyText = `We offer a variety of entertainment from bounce houses to strategy games! Take a look into what our Palace offers.`
+export const WelcomeCardFourLinkText = `Get in touch`
+export const WelcomeCardFourBodyText = `If you have any further questions, comments, or concerns feel free to reach out to us and we'll get back to you!`
+
+
+/* About page */
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,85 +1,118 @@
 <template>
-  <div class="relative ">
-    <img class="object-cover w-full h-52 max-h-52" src="../assets/Play-area-6.webp" alt="Play area with slide, kitchen, horse, and house">
+  <div class="relative">
+    <img
+      class="object-cover w-full h-52 max-h-52"
+      src="../assets/Play-area-6.webp"
+      alt="Play area with slide, kitchen, horse, and house"
+    />
     <div class="mb-8 px-4 pb-12 md:mx-6 md:px-6 lg:mx-8 lg:px-8">
       <div class="text-center my-6 relative">
-        <div class="text-6xl">Welcome to Play Palace Alaska</div>
-          <p
+        <div class="text-6xl">{{ WelcomeTitle }}</div>
+        <p
           class="mt-0 py-6 line-height-5 font-bold text-xl text-center mx-auto text-seconadry"
           style="max-width: 500px"
-          >
-            Lorem ipsum odor amet, consectetuer adipiscing elit. Congue sit lacinia maximus molestie egestas aliquet.
-          </p>
+        >
+          {{WelcomeSubtitle}}
+        </p>
       </div>
-      <div class="flex flex-column flex-wrap gap-6 justify-center w-full ">
-         <div class="card md:max-w-md lg:max-w-lg">
+      <div class="flex flex-column flex-wrap gap-6 justify-center w-full">
+        <div class="card md:max-w-md lg:max-w-lg">
           <div class="card-img">
-            <img class="w-full h-44 object-cover" src="../assets/Rules.jpg" alt="Play Palace Rules">
+            <img
+              class="w-full h-44 object-cover"
+              src="../assets/Rules-2.jpg"
+              alt="Play Palace Rules"
+            />
           </div>
-          <div class="card-body">Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor inceptos molestie mauris rutrum aptent elementum aptent ad? Egestas orci semper aenean lacinia dictum accumsan; et viverra.</div>
-          <!-- <div class="card-footer"></div> -->
+          <div class="card-body">
+            {{WelcomeCardOneBodyText}}
+          </div>
           <div class="card-footer">
-            <a href="/about">Rules and Safety</a>
+            <a href="/about">{{WelcomeCardOneLinkText}}</a>
           </div>
-         </div>
-         <div class="card md:max-w-md lg:max-w-lg">
+        </div>
+        <div class="card md:max-w-md lg:max-w-lg">
           <div class="card-img">
-            <img class="w-full h-44 object-cover" src="../assets/Birthday-3.webp" alt="Play Palace birthday setup">
+            <img
+              class="w-full h-44 object-cover"
+              src="../assets/Birthday-3.webp"
+              alt="Play Palace birthday setup"
+            />
           </div>
-          <div class="card-body">Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor inceptos molestie mauris rutrum aptent elementum aptent ad? Egestas orci semper aenean lacinia dictum accumsan; et viverra.</div>
-          <!-- <div class="card-footer"></div> -->
+          <div class="card-body">
+            {{WelcomeCardTwoBodyText}}
+          </div>
           <div class="card-footer">
-            <a href="/pricing">Pricing</a>
+            <a href="/pricing">{{WelcomeCardTwoLinkText}}</a>
           </div>
-         </div>
-         <div class="card md:max-w-md lg:max-w-lg">
+        </div>
+        <div class="card md:max-w-md lg:max-w-lg">
           <div class="card-img">
-            <img class="w-full h-44 object-cover" src="../assets/Play-area-6.webp" alt="Play Palace play area">
+            <img
+              class="w-full h-44 object-cover"
+              src="../assets/Play-area-6.webp"
+              alt="Play Palace play area"
+            />
           </div>
-          <div class="card-body">Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor inceptos molestie mauris rutrum aptent elementum aptent ad? Egestas orci semper aenean lacinia dictum accumsan; et viverra.</div>
-          <!-- <div class="card-footer"></div> -->
+          <div class="card-body">{{WelcomeCardThreeBodyText}}</div>
           <div class="card-footer">
-            <a href="/gallery">View our Palace</a>
+            <a href="/gallery">{{WelcomeCardThreeLinkText}}</a>
           </div>
-         </div>
-         <div class="card md:max-w-md lg:max-w-lg">
+        </div>
+        <div class="card md:max-w-md lg:max-w-lg">
           <div class="card-img">
-            <img class="w-full h-44 object-cover" src="../assets/info-chart.JPG" alt="Play Palace infomation chart">
+            <img
+              class="w-full h-44 object-cover"
+              src="../assets/Rules-3.JPG"
+              alt="Play Palace infomation chart"
+            />
           </div>
-          <div class="card-body">Lorem ipsum odor amet, consectetuer adipiscing elit. Porttitor inceptos molestie mauris rutrum aptent elementum aptent ad? Egestas orci semper aenean lacinia dictum accumsan; et viverra.</div>
-          <!-- <div class="card-footer"></div> -->
+          <div class="card-body">
+            {{WelcomeCardFourBodyText}}
+          </div>
           <div class="card-footer">
-            <a href="/contact">
-              <span>Get in touch</span></a>
+            <a href="/contact"> <span>{{WelcomeCardFourLinkText}}</span></a>
           </div>
-         </div>
-         
+        </div>
       </div>
-
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {
+  WelcomeTitle,
+  WelcomeSubtitle,
+  WelcomeCardOneLinkText,
+  WelcomeCardOneBodyText,
+  WelcomeCardTwoLinkText,
+  WelcomeCardTwoBodyText,
+  WelcomeCardThreeLinkText,
+  WelcomeCardThreeBodyText,
+  WelcomeCardFourLinkText,
+  WelcomeCardFourBodyText,
+} from "../constants";
+</script>
 
 <style lang="scss" scoped>
 .card {
   display: flex;
   flex-direction: column;
   padding: 6px;
-  // max-width: 400px;
   max-height: 370px;
   border-radius: 0 0 8px 8px;
 
-  .card-img, img {
+  .card-img,
+  img {
     border-radius: 8px 8px 0 0;
   }
-  
-  .card-body, .card-footer {
+
+  .card-body,
+  .card-footer {
     background-color: var(--colors-content_bg);
     padding: 6px 8px;
   }
-  
+
   .card-footer {
     border-radius: 0 0 8px 8px;
     display: flex;
