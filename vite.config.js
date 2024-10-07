8d074fd26d97facc5d736e5bdb79836efaabb816
jhuezo-dev <jessicabhuezo@gmail.com>
2024-10-07 16:56:55 -0700

    adds png image types of vite config file

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  assetsInclude: ["**/*.JPG"]
+  assetsInclude: ["**/*.JPG", "**/*.PNG"]
 })
